43a6895e1c72d41ade70edc64c3e88a25b45cf9d
LipsConviction <158637206+LipsConviction@users.noreply.github.com>
2024-02-04 14:04:18 -0700

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -253,7 +253,7 @@ Booths_Broadcast.OnClientEvent:Connect(function(username, message)
                         return
 		    elseif item == "Magic Shard" and unitGems <= 8000 then
                         coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
-		    elseif item == "Mini Piñata" and unitGems <= 60000 then
+		    elseif item == "Mini Pinata" and unitGems <= 50000 then
                         coroutine.wrap(tryPurchase)(uid, gems, item, version, shiny, amount, username, class, playerid, buytimestamp, listTimestamp)
                         return
 
